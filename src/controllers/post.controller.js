ac2f38c888c400208024f593d9947b6c302857b6
dhruvsolvzz <dhruvgargsad11212@gmail.com>
2026-04-18 15:16:30 +0530

    imageKit fixed

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,19 +1,27 @@
 const postModel = require('../models/post.model')
-import ImageKit, { toFile } from '@imagekit/nodejs';
-
-
-
-const imagekit = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
-});
-
 
 async function createPostController(req, res) {
- const file  = await client.files.upload({
-  file: await toFile(Buffer.from(req.file.Buffer), 'file'),
-  fileName: 'image',
-});
-res.send(file)
+  try {
+    // @imagekit/nodejs v7+ is ESM-only, use dynamic import in a CJS project
+    const { default: ImageKit, toFile } = await import('@imagekit/nodejs')
+
+    const imagekit = new ImageKit({
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+    })
+
+    const file = await imagekit.files.upload({
+      file: await toFile(Buffer.from(req.file.buffer), req.file.originalname),
+      fileName: req.file.originalname,
+      folder: '/posts',
+    })
+
+    res.status(200).json(file)
+  } catch (error) {
+    console.error('Post creation error:', error)
+    res.status(500).json({ msg: 'Failed to create post', error: error.message })
+  }
 }
 
-module.exports = createPostController 
+module.exports = { createPostController }
